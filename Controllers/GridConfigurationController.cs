4cacb7134d52c2778c416c46eb0b6898846bd519
xergon85 <christoffer.isenberg@gmail.com>
2023-05-29 14:03:15 +0200

    Initial commit

---
+++ Controllers/GridConfigurationController.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using StatusGridAPI.Models;
+using StatusGridAPI.Services;
+
+namespace StatusGridAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GridConfigurationController : ControllerBase
+    {
+        private readonly IGridConfigurationService _gridConfigurationService;
+
+        public GridConfigurationController(IGridConfigurationService gridConfigurationService)
+        {
+            _gridConfigurationService = gridConfigurationService;
+        }
+
+        [HttpGet]
+        public IActionResult GetGridConfigurations()
+        {
+            return Ok(_gridConfigurationService.GetAllConfigurations());
+        }
+
+        [HttpGet("{name}")]
+        public IActionResult GetGridConfiguration(string name)
+        {
+            var gridConfiguration = _gridConfigurationService.GetGridConfiguration(name);
+            if (gridConfiguration == null)
+            {
+                return NotFound();
+            }
+            return Ok(gridConfiguration);
+        }
+
+        [HttpPost]
+        public IActionResult SaveGridConfiguration(GridConfiguration gridConfiguration)
+        {
+            _gridConfigurationService.SaveConfiguration(gridConfiguration);
+            return Ok(gridConfiguration);
+        }
+
+        [HttpDelete("{name}")]
+        public IActionResult DeleteGridConfiguration(string name)
+        {
+            var gridConfiguration = _gridConfigurationService.GetGridConfiguration(name);
+            if (gridConfiguration == null)
+            {
+                return NotFound();
+            }
+            _gridConfigurationService.RemoveConfiguration(gridConfiguration);
+            return Ok();
+        }
+
+    }
+}
