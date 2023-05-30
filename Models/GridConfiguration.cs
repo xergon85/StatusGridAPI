8d3dbba11749b79a61c77b753b5a464e47e22669
xergon85 <christoffer.isenberg@gmail.com>
2023-05-30 14:26:06 +0200

    Rename Configuration

--- Models/GridConfiguration.cs
+++ Models/GridConfiguration.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,6 +10,6 @@ namespace StatusGridAPI.Models
     {
         public int Id { get; set; }
         public string Name { get; set; } = "";
-        public ICollection<Status>? Statuses { get; set; }
+        public ICollection<Status> Statuses { get; set; }
     }
 }
