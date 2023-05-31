aab7d9609d6842ae5395a184c702cc828a14274d
xergon85 <christoffer.isenberg@gmail.com>
2023-05-31 10:52:20 +0200

    Prettified StatusCode enum

--- Models/StatusCode.cs
+++ Models/StatusCode.cs
@@ -1,5 +1,8 @@
+using System.Text.Json.Serialization;
+
 namespace StatusGridAPI.Models
 {
+    [JsonConverter(typeof(JsonStringEnumConverter))]
     public enum StatusCode
     {
         Untouched = 0,
