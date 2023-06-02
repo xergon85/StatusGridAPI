358e5a46f50ccc8e178e506a29f77a54f8bdb2ef
xergon85 <christoffer.isenberg@gmail.com>
2023-06-02 09:26:09 +0200

    Back to enum as int

--- Models/StatusCode.cs
+++ Models/StatusCode.cs
@@ -2,7 +2,6 @@ using System.Text.Json.Serialization;
 
 namespace StatusGridAPI.Models
 {
-    [JsonConverter(typeof(JsonStringEnumConverter))]
     public enum StatusCode
     {
         Untouched = 0,
